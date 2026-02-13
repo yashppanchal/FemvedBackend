abd6e2c645c3c482aeee13a755657e0f27e39d97
YashPanchal <yashppanchal@gmail.com>
2026-02-14 03:56:37 +0530

    Added Keys

---
+++ src/FemvedBackend.Api/Configuration/EmailOptions.cs
@@ -0,0 +1,6 @@
+namespace FemvedBackend.Api.Configuration;
+
+public sealed class EmailOptions
+{
+    public string SmtpPassword { get; set; } = string.Empty;
+}
---
+++ src/FemvedBackend.Api/Configuration/PaymentOptions.cs
@@ -0,0 +1,6 @@
+namespace FemvedBackend.Api.Configuration;
+
+public sealed class PaymentOptions
+{
+    public string ApiKey { get; set; } = string.Empty;
+}
